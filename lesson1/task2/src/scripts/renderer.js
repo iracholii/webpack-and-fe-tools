8b4f117c253255d0fc355633ffee2b31ade3d996
iracholii <ira.cholii.m@gmail.com>
2022-05-18 18:24:07 +0300

    adding files

---
+++ lesson1/task2/src/scripts/renderer.js
@@ -0,0 +1,41 @@
+import { getTasksList } from './tasksGateway.js';
+
+export const listElem = document.querySelector('.list');
+
+export const renderTasks = () => {
+  listElem.innerHTML = '';
+
+  const createTaskElem = element => {
+    const listItemElem = document.createElement('li');
+    listItemElem.classList.add('list__item');
+
+    const checkboxElem = document.createElement('input');
+    checkboxElem.setAttribute('type', 'checkbox');
+    checkboxElem.setAttribute('data-id', `${element.id}`);
+    checkboxElem.checked = element.done;
+    checkboxElem.classList.add('list__item-checkbox');
+    if (element.done) {
+      listItemElem.classList.add('list__item_done');
+    }
+
+    const textElem = document.createElement('span');
+    textElem.classList.add('list__item-text');
+    textElem.textContent = element.text;
+
+    const deleteButtonElem = document.createElement('button');
+    deleteButtonElem.classList.add('list__item-delete-button');
+    deleteButtonElem.setAttribute('data-id', `${element.id}`);
+    deleteButtonElem.textContent = '+';
+
+    listItemElem.append(checkboxElem, textElem, deleteButtonElem);
+
+    return listItemElem;
+  };
+
+  const tasksList = getTasksList();
+  tasksList
+    .then(tasks => tasks.sort((a, b) => a.done - b.done).map(createTaskElem))
+    .then(elem => {
+      listElem.append(...elem);
+    });
+};
